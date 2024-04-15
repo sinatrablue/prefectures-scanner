45ce147c537f5479f7b644182477e5f998fe1d7f
Robin Simonot <simonot.robin@gmail.com>
2024-04-15 13:23:48 +0200

    Experimented with tailwind

--- app/components/PageControls/PageControls.tsx
+++ app/components/PageControls/PageControls.tsx
@@ -3,7 +3,12 @@ import { PageControlsProps } from "./types";
 export function PageControls({ updateResults }: Readonly<PageControlsProps>) {
   return (
     <div className="flex items-between mb-20">
-      <button onClick={updateResults}>Get results</button>
+      <button
+        className=" px-5 py-2 bg-violet-300 hover:bg-violet-400"
+        onClick={updateResults}
+      >
+        Get results
+      </button>
     </div>
   );
 }
