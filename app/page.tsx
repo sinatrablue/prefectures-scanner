faa87996b50e905621b9683f843e5a3e34c74de2
Robin Simonot <simonot.robin@gmail.com>
2024-05-02 14:51:37 +0200

    Just some styles

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,12 @@ import MainFrame from "./MainFrame/page";
 export default function Home() {
   return (
     <>
-      <div className="flex justify-center py-5">
+      <div className="flex justify-center py-5 shadow-sm">
         <h1 className="font-bold text-2xl text-violet-950">
           PREFECTURES SCANNER
         </h1>
       </div>
-      <main className="flex flex-col items-center justify-between px-5 py-8">
+      <main className="flex flex-col items-center px-5 py-8 bg-gray-50 h-screen">
         <MainFrame />
       </main>
     </>
